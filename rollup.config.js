57da4eb4c8263744634df51ddb84c2eb12d84a2a
valleykid <valleykiddy@gmail.com>
2022-06-24 10:39:25 +0800

    chore: add build/ci toolkit

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import commonjs from '@rollup/plugin-commonjs';
+import injectProcessEnv from 'rollup-plugin-inject-process-env';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import dts from "rollup-plugin-dts";
+import * as pkg from './package.json';
+
+const extensions = ['.js', '.ts'];
+const plugins = [
+  commonjs(),
+  nodeResolve({ extensions }),
+  babel({ extensions, include: ['./src/**/*'] }),
+  injectProcessEnv({ NODE_ENV: process.env.NODE_ENV }),
+  terser(),
+];
+
+const umdConfig = {
+  input: 'src/index.ts',
+  output: { dir: 'lib', name: 'proxySandboxMidware', format: 'umd' },
+  plugins,
+};
+
+const esConfig = {
+  input: 'src/index.ts',
+  external: Object.keys(pkg.dependencies),
+  output: { file: 'lib/index.es.js', format: 'es' },
+  plugins,
+};
+
+const dtsConfig = {
+  input: './src/index.ts',
+  output: { file: './lib/index.d.ts', format: 'es' },
+  plugins: [dts()]
+}
+
+export default [umdConfig, esConfig, dtsConfig];
